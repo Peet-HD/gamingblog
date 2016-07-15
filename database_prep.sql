b8a787f0c24df2922b212e56421acebec8fd6c87
Luckysh33p <>
2016-07-15 14:52:11 +0200

    SQL-Collection reorganized
    - Corrected small bugs in the sql-order and syntax

--- database_prep.sql
+++ database_prep.sql
@@ -11,23 +11,27 @@
 ------------------------------------------------------------------------------
 -- Benutzer-ADMIN-Tabelle
 
--- Tabellenstruktur für Tabelle `user_admin`
+DROP TABLE IF EXISTS `blog_entry`;
+DROP TABLE IF EXISTS `blog_category`;
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `chat_data`;
 DROP TABLE IF EXISTS `user_admin`;
+DROP TABLE IF EXISTS `user_visitor`;
+
 CREATE TABLE `user_admin` (
  `adminId` int(11) unsigned NOT NULL AUTO_INCREMENT,
  `userName` varchar(40) NOT NULL,
  `password` varchar(100) NOT NULL,
  `email` varchar(100) NOT NULL,
  `adminLevel` tinyint(3) unsigned NOT NULL,
- `active` bit(1) NOT NULL,
+ `active` tinyint(1) NOT NULL,
  PRIMARY KEY (`adminId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
 -- Benutzer-BESUCHER-Tabelle
 
 -- Tabellenstruktur für Tabelle `user_visitor`
-DROP TABLE IF EXISTS `user_visitor`;
 CREATE TABLE `user_visitor` (
  `userId` int(11) unsigned NOT NULL AUTO_INCREMENT,
  `userName` varchar(40) NOT NULL,
@@ -35,13 +39,19 @@ CREATE TABLE `user_visitor` (
  `email` varchar(100) NOT NULL,
  `active` tinyint(1) NOT NULL DEFAULT '0',
  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------
+
+-- BLOG-Category-Tabelle
+CREATE TABLE `blog_category` (
+ `categoryId` int(10) unsigned NOT NULL,
+ `categoryName` varchar(60) NOT NULL,
+ PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- BLOG-Eintrags-Tabelle
-
 -- Tabellenstruktur für Tabelle `blog_entry`
-DROP TABLE IF EXISTS `blog_entry`;
 CREATE TABLE `blog_entry` (
  `blogId` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `title` varchar(100) NOT NULL,
@@ -54,14 +64,7 @@ CREATE TABLE `blog_entry` (
  KEY `categoryId` (`categoryId`),
  CONSTRAINT `admin_id_fk` FOREIGN KEY (`adminId`) REFERENCES `user_admin` (`adminId`) ON UPDATE CASCADE,
  CONSTRAINT `category_id_fk` FOREIGN KEY (`categoryId`) REFERENCES `blog_category` (`categoryId`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
--- BLOG-Category-Tabelle
-CREATE TABLE `blog_category` (
- `categoryId` int(10) unsigned NOT NULL,
- `categoryName` varchar(60) NOT NULL,
- PRIMARY KEY (`categoryId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `comment` (
  `commentID` int(11) NOT NULL,
@@ -71,12 +74,11 @@ CREATE TABLE `comment` (
  `Name` varchar(50) NOT NULL,
  `Email` varchar(200) DEFAULT NULL,
  PRIMARY KEY (`commentID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-------------------------------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------------------------------------------------------
 -- CHAT-Tabelle
 
 -- Tabellenstruktur für Tabelle `chat_data`
-DROP TABLE IF EXISTS `chat_data`;
 CREATE TABLE `chat_data` (
  `entryId` int(11) unsigned NOT NULL AUTO_INCREMENT,
  `userId` int(11) NOT NULL,
@@ -84,4 +86,4 @@ CREATE TABLE `chat_data` (
  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  `text` varchar(50) NOT NULL,
  PRIMARY KEY (`entryId`)
-) ENGINE=InnoDB AUTO_INCREMENT=709 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=709 DEFAULT CHARSET=utf8;
